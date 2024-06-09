5a61283134830e64b859db16b07b4c4f299cb92c
BA <developer@3371.online>
2024-06-10 01:12:41 +0800

    Coroutine is no longer needed

--- ExamplePlugin.cs
+++ ExamplePlugin.cs
@@ -14,19 +14,15 @@ namespace UpdateNotifier
 
         public void TryCheckUpdate ()
         {
-            StartCoroutine(CheckUpdate());
-        }
-
-        IEnumerator CheckUpdate ()
-        {
             if (!Chainloader.PluginInfos.TryGetValue("bastudio.updatenotifier", out var pluginInfo))
             {
                 Logger.LogInfo("Update Notifier not found.");
-                yield break;
+                return;
             }
 
             BaseUnityPlugin updntf = pluginInfo.Instance;
             updntf.GetType().GetMethod("CheckForUpdate").Invoke(updntf, new object[] {this, UPDATE_NOTIFIER_VERSION_FILE_URL});
+        
         }
     }
 }
