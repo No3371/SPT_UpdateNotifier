81b7e1a4b7e91e0ff1e516fa7ba52fea779f3900
BA <developer@3371.online>
2024-06-16 20:13:44 +0800

    Supports server mods

--- UpdateNotifierPlugin.cs
+++ UpdateNotifierPlugin.cs
@@ -7,9 +7,28 @@ using System.Collections.Generic;
 using UnityEngine.Networking;
 using System.Collections;
 using EFT.UI;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Concurrent;
 
 namespace UpdateNotifier
 {
+    public struct QueuedCheck
+    {
+        public readonly string name;
+        public readonly string id;
+        public readonly Version version;
+        public readonly string remote;
+
+        public QueuedCheck(string name, string id, Version version, string remote)
+        {
+            this.name = name;
+            this.id = id;
+            this.version = version;
+            this.remote = remote;
+        }
+    }
+
     public static class AssemblyInfo
     {
         public const string Title = ModName;
@@ -35,70 +54,106 @@ namespace UpdateNotifier
     [DefaultExecutionOrder(100)]
     public class UpdateNotifierPlugin : BaseUnityPlugin
     {
-        Queue<(BaseUnityPlugin, string)> queue;
+        ConcurrentQueue<QueuedCheck> queue;
         ConfigEntry<bool> Enabled { get; set; }
         ConfigEntry<string> Whitelist { get; set; }
         Coroutine checking;
+        int updatesAvailable;
         void Awake ()
         {
             Config.Bind("Main", "Readme", false, "By enabling the plugin, you agree to allow the plugin to fetch version info from the internet on your behalf, when a mod request to check .");
             Enabled = Config.Bind("Main", "Enabled", false, "By enabling the plugin, you agree to allow the plugin to fetch version info from the internet on your behalf, when a mod request to check .");
             Whitelist = Config.Bind("Main", "Whitelist", "");
 
+            running = CheckSourceFiles();
             CheckForUpdate(this, "https://raw.githubusercontent.com/No3371/SPT_UpdateNotifier/master/.update_notifier");
+        }
+
+        void FixedUpdate ()
+        {
+            if (CommonUI.Instance?.MenuScreen?.isActiveAndEnabled == true
+             && !queue.IsEmpty)
+            {
+                
+                if (checking == null)
+                    checking = StartCoroutine(CheckWorker());
+            }
         }
 
         public void CheckForUpdate(BaseUnityPlugin plugin, string versionFileUrl)
         {
             if (!Enabled.Value) return;
             if (queue == null)
-                queue = new Queue<(BaseUnityPlugin, string)>();
+                queue = new ();
 
-            queue.Enqueue((plugin, versionFileUrl));
+            queue.Enqueue(new QueuedCheck(plugin.Info.Metadata.Name, plugin.Info.Metadata.GUID, plugin.Info.Metadata.Version, versionFileUrl));
 
             if (checking == null)
                 checking = StartCoroutine(CheckWorker());
         }
+        public void CheckForUpdate(string name, string id, Version version, string remote)
+        {
+            if (!Enabled.Value) return;
+            if (queue == null)
+                queue = new ();
+
+            queue.Enqueue(new QueuedCheck(name, id, version, remote));
+
+            if (checking == null)
+                checking = StartCoroutine(CheckWorker());
+        }
+        public void QueueCheckForUpdate(string name, string id, Version version, string remote)
+        {
+            if (!Enabled.Value) return;
+            if (queue == null)
+                queue = new ();
+
+            queue.Enqueue(new QueuedCheck(name, id, version, remote));
+        }
         bool inDialog;
         IEnumerator CheckWorker ()
         {
-            (BaseUnityPlugin, string) task;
+            QueuedCheck task;
             var wait = new WaitForSeconds(1f);
             while (!PreloaderUI.Instantiated || CommonUI.Instance?.MenuScreen?.isActiveAndEnabled != true)
                 yield return wait;
+
             while (queue.TryDequeue(out task))
             {
                 while (inDialog)
                     yield return wait;
-                yield return Check(task.Item1, task.Item2);
+                yield return Check(task.name, task.id, task.version, task.remote);
                 yield return wait;
             }
+            string message = $"[Update Notifier] Updates available for {updatesAvailable} mods. Please check console. (~)";
+            NotificationManagerClass.DisplayWarningNotification(message, EFT.Communications.ENotificationDurationType.Long);
+            // EFT.UI.ConsoleScreen.Log(message);
             checking = null;
         }
 
-        IEnumerator Check(BaseUnityPlugin plugin, string versionFileUrl)
+        IEnumerator Check(string name, string id, Version version, string versionFileUrl)
         {
-            EFT.UI.ConsoleScreen.Log($"[Update Notifier] {plugin.Info.Metadata.Name} requested to check version file at {versionFileUrl}");
+            EFT.UI.ConsoleScreen.Log($"[Update Notifier] {name} requested to check version file at {versionFileUrl}");
 
             if (!versionFileUrl.StartsWith("https://"))
             {
-                string msg = $"Invalid versionFileUrl requested by {plugin.Info.Metadata.Name} ({plugin.Info.Metadata.GUID}). Version file URL must starts with https://";
+                string msg = $"Invalid versionFileUrl requested by {name}. Version file URL must starts with https://";
                 NotificationManagerClass.DisplayWarningNotification(msg, EFT.Communications.ENotificationDurationType.Long);
                 EFT.UI.ConsoleScreen.LogError(msg);
                 yield break;
             }
             if (!versionFileUrl.EndsWith(".update_notifier"))
             {
-                string msg = $"Invalid versionFileUrl requested by {plugin.Info.Metadata.Name} ({plugin.Info.Metadata.GUID}). Version file URL must end with .update_notifier";
+                string msg = $"Invalid versionFileUrl requested by {name}. Version file URL must end with .update_notifier";
                 NotificationManagerClass.DisplayWarningNotification(msg, EFT.Communications.ENotificationDurationType.Long);
                 EFT.UI.ConsoleScreen.LogError(msg);
                 yield break;
             }
 
-            if (!Whitelist.Value.Contains($"{plugin.Info.Metadata.GUID}=>{versionFileUrl}"))
+            if (!Whitelist.Value.Contains($"{id}=>{versionFileUrl}"))
             {
                 inDialog = true;
-                var msg = $"{plugin.Info.Metadata.Name} ({plugin.Info.Metadata.GUID}) is requesting to download unknown file from {versionFileUrl} to check for new version, but the url is not in white list.\n\nDo you want to allow the request and add it to whitelist?\n\n This would allows Update Notifier to remind you if it finds a update for the mod from now on.";
+                var msg = $"{name} ({id}) is requesting to download unknown file from {versionFileUrl} to check for new version, but the url is not in white list.\n\nDo you want to allow the request and add it to whitelist?\n\n This would allows Update Notifier to remind you if it finds a update for the mod from now on.";
                 ItemUiContext.Instance.ShowMessageWindow(msg, PreAccept, Decline, "Update Notifier");
                 yield break;
             }
@@ -111,7 +166,7 @@ namespace UpdateNotifier
 
                 if (webRequest.isNetworkError || webRequest.isHttpError)
                 {
-                    var msg = $"[Update Notifier] Failed to fetch version file requested by {plugin.Info.Metadata.Name}:{webRequest.error}";
+                    var msg = $"[Update Notifier] Failed to fetch version file requested by {name}:{webRequest.error}";
                     EFT.UI.ConsoleScreen.LogError(msg);
                     Logger.LogError(msg);
                 }
@@ -120,43 +175,104 @@ namespace UpdateNotifier
                     var text = webRequest.downloadHandler.text;
                     if (webRequest.downloadedBytes > 16)
                     {
-                        string message = $"[Update Notifier] {plugin.Info.Metadata.Name} requested a suspicious version file! Update Notifier only accepts up to 16 bytes: {text}";
+                        string message = $"[Update Notifier] {name} ({id}) requested a suspicious version file! Update Notifier only accepts up to 16 bytes: {text}";
                         EFT.UI.ConsoleScreen.LogWarning(message);
                         yield break;
                     }
                     if (!Version.TryParse(text, out var ver))
                     {
-                        string message = $"[Update Notifier] {plugin.Info.Metadata.Name} requested a invalid version file! The content is not a valid version string: {text} ({text.Length})";
+                        string message = $"[Update Notifier] {name} ({id}) requested a invalid version file! The content is not a valid version string: {text} ({text.Length})";
                         EFT.UI.ConsoleScreen.LogWarning(message);
                         yield break;
                     }
-                    else if (ver.CompareTo(plugin.Info.Metadata.Version) > 0)
+                    else if (ver.CompareTo(version) > 0)
                     {
-                        string message = $"[Update Notifier] Update for {plugin.Info.Metadata.Name} is available: {text} (Current: {plugin.Info.Metadata.Version})";
-                        NotificationManagerClass.DisplayWarningNotification(message, EFT.Communications.ENotificationDurationType.Long);
+                        string message = $"[Update Notifier] Update for {name} is available: {text} (Current: {version})";
+                        // NotificationManagerClass.DisplayWarningNotification(message, EFT.Communications.ENotificationDurationType.Long);
                         EFT.UI.ConsoleScreen.Log(message);
+                        updatesAvailable++;
                     }
                 }
             }
 
             void PreAccept ()
             {
-                var msg = $"Are you sure you want to allow Update Notifier to fetch the requested version file over the internet?\n\nUpdate Notifier is not responsible in case the url links to malicious servers.\n\n{plugin.Info.Metadata.Name} ({plugin.Info.Metadata.GUID})\n{versionFileUrl}";
+                var msg = $"Are you sure you want to allow Update Notifier to fetch the requested version file over the internet?\n\nUpdate Notifier is not responsible in case the url links to malicious servers. A malicious server may collect your IP or sort like how ad companies do.\n\nRequesting:\n\n{name} ({id})\n{versionFileUrl}";
                 ItemUiContext.Instance.ShowMessageWindow(msg, Accept, Decline, "Update Notifier");
             }
 
             void Accept ()
             {
                 inDialog = false;
-                Whitelist.Value += $"{plugin.Info.Metadata.GUID}=>{versionFileUrl}, ";
-                CheckForUpdate(plugin, versionFileUrl);
+                Whitelist.Value += $"{id}=>{versionFileUrl}, ";
+                CheckForUpdate(name, id, version, versionFileUrl);
             }
 
             void Decline ()
             {
                 inDialog = false;
-                EFT.UI.ConsoleScreen.Log($"[Update Notifier] User cancelled request from {plugin.Info.Metadata.Name}.");
+                EFT.UI.ConsoleScreen.Log($"[Update Notifier] User cancelled request from {name} ({id}).");
             }
         }
+
+        Task running;
+        public Task CheckSourceFiles ()
+        {
+            if (running != null && running.IsCompleted == false)
+                return running;
+            running = Task<IEnumerable<string>>.Run(() => {
+                List<(string, string)> loaded = new List<(string, string)>();
+                IEnumerable<string> serverMods = Directory.EnumerateDirectories($"{BepInEx.Paths.GameRootPath}/user/mods");
+                foreach (var dir in serverMods)
+                {
+                    string srcFile = Path.Combine(dir, ".update_notifier_source");
+                    if (!File.Exists(srcFile))
+                        continue;
+
+                    string v = File.ReadAllText(srcFile);
+                    if (string.IsNullOrWhiteSpace(v))
+                    {
+                        var message = $"[Update Notifier] Invalid .update_notifier_source: { srcFile }";
+                        Logger.LogError(message);
+                        continue;
+                    }
+                    loaded.Add((srcFile, v));
+                    Logger.LogInfo($"[Update Notifier] Found source file: {srcFile}");
+                }
+
+                foreach (var src in loaded)
+                {
+                    var packageJson = File.ReadAllText(Path.Combine(Path.GetDirectoryName(src.Item1), "package.json"));
+                    ServerPackage package = Newtonsoft.Json.JsonConvert.DeserializeObject<ServerPackage>(packageJson);
+                    if (package == null)
+                    {
+                        var message = $"[Update Notifier] Invalid package.json: { packageJson }";
+                        Logger.LogError(message);
+                        continue;
+                    }
+                    package.remote = src.Item2;
+                    if (!Version.TryParse(package.version, out var ver))
+                    {
+                        EFT.UI.ConsoleScreen.Log($"[Update Notifier] Invalid version string: {package.version} ({src.Item1})");
+                        continue;
+                    }
+                    QueueCheckForUpdate(package.name, $"{package.author}.{package.name}", ver, package.remote);
+                }
+                
+            }).ContinueWith(t => {
+                running = null;
+                if (t.IsFaulted) Logger.LogError(t.Exception.Flatten());
+            });
+
+            return running;
+        }
+
+    }
+    public class ServerPackage
+    {
+        public string name { get; set; }
+        public string author   { get; set; }
+        public string version { get; set; }
+        public string remote { get; set; }
     }
 }
